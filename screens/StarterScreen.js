6e0104e66693f28ff3dc34e041b5597bcae60a62
Yander alves <yanderceara@gmail.com>
2023-07-27 22:21:25 -0300

    Finalização da página Start

--- screens/StarterScreen.js
+++ screens/StarterScreen.js
@@ -1,32 +1,99 @@
-import { View, StatusBar, Dimensions, Image } from "react-native";
-import React from "react";
-import { Carousel } from "react-native-snap-carousel";
-import coffees from "../constants";
+import {
+  Dimensions,
+  ImageBackground,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState, useRef } from "react";
+import { Carousel, Pagination } from "react-native-snap-carousel";
 
-const { width, height } = Dimensions.get("screen");
+const carouselMessages = [
+  " The best grain, the finest roast, the most powerful flavor.",
+  "Awakening the senses with each velvety sip, coffee unveils a symphony of flavors, a warm embrace that soothes the soul.",
+  "From dawn's first light to dusk's embrace, coffee weaves its magic, energizing minds and fostering moments of connection.",
+];
 
-export default function Starter() {
+const { width } = Dimensions.get("window");
+
+export const SLIDER_WIDTH = width + 80;
+export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
+
+export default function Starter({ navigation }) {
+  const [index, setIndex] = useState(0);
+  const isCarousel = useRef(null);
   return (
-    <View className="h-2/4">
+    <ImageBackground
+      source={require("../assets/coffees/robbie-down-LI8inyHnm_A-unsplash.jpg")}
+      className="px-2 pb-14 flex-1 bg-[#0C0F14] justify-end items-center"
+      imageStyle={{ opacity: 0.3 }}
+    >
+      <Text
+        numberOfLines={3}
+        className="text-white leading-10 text-center font-bold w-52 text-3xl"
+      >
+        Good coffee, Good Friends, let it blends
+      </Text>
       <Carousel
-        data={coffees}
-        itemWidth={width}
-        sliderWidth={width}
+        layout="default"
+        data={carouselMessages}
+        itemWidth={ITEM_WIDTH}
+        sliderWidth={SLIDER_WIDTH}
+        inactiveSlideOpacity={0}
+        ref={isCarousel}
+        onSnapToItem={(index) => setIndex(index)}
+        contentContainerCustomStyle={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        containerCustomStyle={{
+          flexGrow: 0,
+        }}
         renderItem={({ item }) => (
-          <View>
-            <Image
-              resizeMode="cover"
-              className="w-screen h-full px-8"
-              source={item.image}
-            />
+          <View className="mt-5">
+            <Text
+              numberOfLines={3}
+              className="text-[#939eb6] text-base font-semibold text-center"
+            >
+              {item}
+            </Text>
           </View>
         )}
-        useScrollView={true}
-        containerCustomStyle={{
-          backgroundColor: "#0C0F14",
-          height: height / 2,
-        }}
       ></Carousel>
-    </View>
+      <Pagination
+        dotsLength={carouselMessages.length}
+        containerStyle={{
+          flexGrow: 0,
+          justifyContent: "center",
+          alignItems: "center",
+          height: 15,
+          width: 80,
+          gap: 15,
+        }}
+        activeDotIndex={index}
+        carouselRef={isCarousel}
+        dotStyle={{
+          width: 12,
+          height: 12,
+          borderRadius: 5,
+          marginHorizontal: 0,
+          backgroundColor: "#D17742",
+        }}
+        inactiveDotOpacity={0.7}
+        inactiveDotScale={0.6}
+        tappableDots={true}
+      />
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("home")}
+        className="bg-[#D17742]  rounded-full px-20 py-3"
+      >
+        <Text className="text-white text-center font-semibold text-3xl">
+          Get started
+        </Text>
+      </TouchableOpacity>
+      <StatusBar />
+    </ImageBackground>
   );
 }
